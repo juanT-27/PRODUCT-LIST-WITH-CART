217e95edd6847baef67372c204af33a9028c7092
juanT-27 <jtsoftdeveloper@gmail.com>
2024-07-24 03:23:44 -0500

    creating a separate class for the cart

--- app.js
+++ app.js
@@ -1,120 +1,103 @@
-class StoreUserInterface {
-  constructor(catalogContainer, template, cartContainer, cartTemplate) {
-    this.catalogContainer = catalogContainer;
-    this.template = template;
+class Cart {
+  constructor(cartContainer, cartTemplate) {
     this.cartContainer = cartContainer;
     this.cartTemplate = cartTemplate;
-    this.cartList = [];
-    this.cartTotal = 0;
-    this.products;
+    this.total = 0;
+    this.__cartList = [];
+  }
+  get cartList() {
+    console.log(this.__cartList);
   }
 
-  findProduct(obj, productId, list) {
-    let search = obj.findIndex(
-      (el) => (list === "products" ? el.name : el.id) === productId
-    );
 
-    if (search !== -1) {
-      if (list === "cart") {
-        this.cartList[search]["units"] += 1;
-        return search;
-      }
-      return search;
+  findProductIndex(product, arr) {
+    return arr.findIndex((el) => el.name === product);
+  }
+
+  multiplyValues(price, units){
+    return price*units
+  }
+
+  manageProductUnits (indx){
+    let element = this.__cartList[indx];
+    element.units += 1;
+    element.totalPrice= this.multiplyValues(element.price, element.units);
+    this.cartList
+  }
+
+  handleAddOrRestClicked(prodId, objClicked){
+    console.log(prodId)
+  }
+
+  createProductCard(product){
+    let template= document.importNode(this.cartTemplate, true).content
+    template.querySelector("#title").textContent= product.name
+    template.querySelector("#units").textContent= product.units
+    template.querySelector("#productPrice").textContent= `$${product.price}`
+    template.querySelector("#totalPrice").textContent= `*${product.units} $${product.totalPrice}`
+    let addBtn= template.querySelector("#plusOne")
+    let restBtn= template.querySelector("#minuOne")
+    addBtn.setAttribute("productId", product.name)
+    restBtn.setAttribute("productId", product.name)
+    return template
+  }
+
+  displayProducts(){
+    this.__cartList.forEach(product =>{
+     let card= this.createProductCard(product)
+     this.cartContainer.appendChild(card)
+    })
+  }
+  checkRepetition(product) {
+    let productIndex = this.findProductIndex(product, this.__cartList);
+    if (productIndex !== -1) {
+     this.manageProductUnits(productIndex)
+      return true
     }
-    return null;
+    return false
   }
 
-  getTotalPrice(units, price) {
-    let result = parseInt(units) * parseFloat(price);
-    return result;
+  addProduct(product, prodPrice) {
+    if (this.checkRepetition(product)) return;
+    let productAdded = { name: product, units: 1, price: parseFloat(prodPrice), };
+    productAdded.totalPrice= this.multiplyValues(productAdded.price, productAdded.units);
+    this.__cartList.push(productAdded);
+    this.cartList;
+    this.displayProducts()
   }
-  findIndexInCart(productId) {
-    return this.cartList.findIndex((product) => product.id == productId);
-  }
-  handleUnitsInCart(productInCart, action) {
-    let productClicked = this.findIndexInCart(productInCart);
-    if (action === "minusOne" && productClicked !== -1) {
-      this.cartList[productClicked].units -= 1;
-      if (this.cartList[productClicked].units === 0) {
-        this.cartList.splice(productClicked, 1);
-      }
-    } else if (action === "plusOne" && productClicked !== -1) {
-      this.cartList[productClicked].units += 1;
-    }
-    this.showProductsInCart();
+}
+
+class Store {
+  constructor(catalogContainer, template) {
+    this.catalogContainer = catalogContainer;
+    this.template = template;
+    this.products = [];
   }
 
-  showProductsInCart() {
-    this.cartContainer.innerHTML = "";
-    this.cartList.forEach((product) => {
-      let dataProduct = this.products[product["id"]];
-      let cartProductTemplate = document.importNode(
-        this.cartTemplate.content,
-        true
-      );
-      cartProductTemplate.querySelector("#title").textContent =
-        dataProduct.name;
-      cartProductTemplate.querySelector("#units").textContent =
-        product["units"];
-      cartProductTemplate.querySelector(
-        "#productPrice"
-      ).textContent = `Price = $${dataProduct.price.toFixed(2)}`;
-      cartProductTemplate.querySelector("#totalPrice").textContent = `*${
-        product["units"]
-      } = ${this.getTotalPrice(product["units"], dataProduct.price).toFixed(
-        2
-      )}`;
-      let restProductUnitsBtn = cartProductTemplate.querySelector("#minusOne");
-      let addProductUnitsBtn = cartProductTemplate.querySelector("#plusOne");
-      restProductUnitsBtn.setAttribute("productId", product["id"]);
-      addProductUnitsBtn.setAttribute("productId", product["id"]);
+  createProductsCard(product) {
+    let productTemplate = document.importNode(this.template.content, true);
+    let productImg = productTemplate.querySelector("#productImg");
+    productImg.setAttribute("src", product.image["mobile"]);
+    productImg.setAttribute("alt", product.image["name"]);
+    let addProduct = productTemplate.querySelector("#addBtn");
+    productTemplate.querySelector("#productCategory").textContent =
+      product.category;
+    productTemplate.querySelector("#productName").textContent = product.name;
+    productTemplate.querySelector(
+      "#productPrice"
+    ).textContent = `$${product.price.toFixed(2)}`;
 
-      this.cartContainer.appendChild(cartProductTemplate);
+    addProduct.addEventListener("click", (e) => {
+      cart.addProduct(product.name, product.price.toFixed(2));
     });
-  }
 
-  addToCartList(productName) {
-    let productIndex = this.findProduct(this.products, productName, "products");
-    let newProductInCart = {};
-    if (productIndex !== null) {
-      let productRepeated = this.findProduct(
-        this.cartList,
-        productIndex,
-        "cart"
-      );
-
-      if (productRepeated !== null) {
-        this.showProductsInCart();
-        return;
-      }
-      newProductInCart["id"] = productIndex;
-      newProductInCart["units"] = 1;
-      this.cartList.push(newProductInCart);
-      console.log(this.cartList);
-      this.showProductsInCart();
-      return;
-    }
+    return productTemplate;
   }
 
   displayProducts(products) {
-    console.log(products);
     products.forEach((product) => {
-      let productTemplate = document.importNode(this.template.content, true);
-      let productImg = productTemplate.querySelector("#productImg");
-      productImg.setAttribute("src", product.image["mobile"]);
-      productImg.setAttribute("alt", product.image["name"]);
-      let addProduct = productTemplate.querySelector("#addBtn");
-      productTemplate.querySelector("#productCategory").textContent =
-        product.category;
-      productTemplate.querySelector("#productName").textContent = product.name;
-      productTemplate.querySelector(
-        "#productPrice"
-      ).textContent = `$${product.price.toFixed(2)}`;
-      this.catalogContainer.appendChild(productTemplate);
-
-      addProduct.addEventListener("click", (e) => {
-        this.addToCartList(product.name);
-      });
+      let productCard = this.createProductsCard(product);
+      this.catalogContainer.appendChild(productCard);
     });
   }
 
@@ -125,33 +108,35 @@ class StoreUserInterface {
         throw new Error(`Error al cargar archivor json`);
       }
       const products = await response.json();
-
       this.products = products;
       this.displayProducts(products);
+      return products;
     } catch (error) {
       console.log(error);
     }
   }
 
   storeEvents() {
-    document.addEventListener("DOMContentLoaded", () => {
-      this.loadProducts();
+    document.addEventListener("DOMContentLoaded", async () => {
+      await this.loadProducts();
     });
     document.addEventListener("click", (e) => {
       let productId = e.target.getAttribute("productId");
       let objClicked = e.target.getAttribute("id");
       if (objClicked === "minusOne" || objClicked === "plusOne") {
-        this.handleUnitsInCart(productId, objClicked);
+        cart.handleAddOrRestClicked(productId, objClicked);
       }
     });
   }
 }
 
-const store = new StoreUserInterface(
+const store = new Store(
   document.querySelector("#catalog"),
-  document.querySelector("#productTemplate"),
+  document.querySelector("#productTemplate")
+);
+
+const cart = new Cart(
   document.querySelector("#productsInCart"),
   document.querySelector("#cartTemplate")
 );
-
 store.storeEvents();
